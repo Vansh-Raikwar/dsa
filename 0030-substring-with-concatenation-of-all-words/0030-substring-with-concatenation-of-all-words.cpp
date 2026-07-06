78f50ca34a9edccb6c8e07b9eb1cb3dad42191d6
Vansh <143195512+Vansh-Raikwar@users.noreply.github.com>
2026-07-06 16:59:20 +0530

    Time: 31 ms (23.92%), Space: 24 MB (25.43%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int m = words[0].size();
+        int n = words.size();
+        int tl = n*m;
+        unordered_map<string,int> mp1;
+        vector<int> ans;
+        for(auto w:words){
+            mp1[w]++;
+        }
+        if(tl>s.size()){
+            return ans;
+        }
+        for(int i=0;i<m;i++){
+            unordered_map<string,int> mp2;
+            int left=i;
+            for(int j=i;j<=s.size()-m;j+=m){
+                string temp = s.substr(j,m);
+                if(mp1.find(temp)!=mp1.end()){
+                    mp2[temp]++;
+                    while(mp2[temp]>mp1[temp]){
+                        mp2[s.substr(left,m)]--;
+                        left+=m;
+                    }
+                    if(j-left+m==tl){
+                        ans.push_back(left);
+                        mp2[s.substr(left,m)]--;
+                        left+=m;
+                    }
+                }
+                else{
+                    mp2.clear();
+                    left=j+m;
+                }
+            }
+        }
+        return ans;
+    }
+};
