afb75ceed2c10f450338b1ccd02ed647ed216c13
Vansh <143195512+Vansh-Raikwar@users.noreply.github.com>
2026-07-07 15:40:57 +0530

    Time: 0 ms (100%), Space: 26.2 MB (68.67%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,14 +6,13 @@ public:
         int st = 0, end=n-1;
         while(st<end){
             int mid = st+(end-st)/2;
-            if(mid==0 && nums[1]!=nums[0]) return nums[0];
-            if(mid==n-1 && nums[n-1]!=nums[n-2]) return nums[n-1];
-            if(nums[mid]!=nums[mid-1]&& nums[mid]!=nums[mid+1]) return nums[mid];
-            if(mid%2==0){   // even case
-                (nums[mid] == nums[mid+1]) ? st = mid + 2 : end = mid;
-            }else{     // odd case
-                
-                (nums[mid-1] == nums[mid]) ? st = mid + 1 : end = mid - 1;
+            if(mid%2==1){
+                mid--;
+            }
+            if(nums[mid]==nums[mid+1]){
+                st = mid+2;
+            }else{
+                end=mid;
             }
         }
         return nums[st];
