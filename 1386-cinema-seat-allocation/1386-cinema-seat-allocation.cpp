1780a0e724fbbd613489c82e2dcf51e657531801
Vansh <143195512+Vansh-Raikwar@users.noreply.github.com>
2026-08-19 23:50:26 +0530

    Time: 63 ms (40.88%), Space: 66.8 MB (47.64%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int, set<int>> mp;
+        unordered_map<int,int> mp;
         for(auto el:reservedSeats){
             int row = el[0];
             int seat = el[1];
-            mp[row].insert(seat);
+            mp[row] = (mp[row]|(1<<seat));
         }
         int result = (n-mp.size())*2;
-        for(auto [row,seats]:mp){
-
-            auto isAvalible = [&](int a){
-                return seats.find(a) == seats.end();
-            };
-
-            bool A = isAvalible(2)&&isAvalible(3)&&isAvalible(4)&&isAvalible(5);
-            bool B = isAvalible(4)&&isAvalible(5)&&isAvalible(6)&&isAvalible(7);
-            bool C = isAvalible(6)&&isAvalible(7)&&isAvalible(8)&&isAvalible(9);
+        int mask_A = (1<<2)|(1<<3)|(1<<4)|(1<<5);
+        int mask_B = (1<<4)|(1<<5)|(1<<6)|(1<<7);
+        int mask_C = (1<<6)|(1<<7)|(1<<8)|(1<<9);
+        for(auto [row,booked_mask]:mp){
+            bool A = (mask_A & booked_mask)==0;
+            bool B = (mask_B & booked_mask)==0;
+            bool C = (mask_C & booked_mask)==0;
             if(A && C) result+=2;
             else if(A||B||C) result+=1;
         }
